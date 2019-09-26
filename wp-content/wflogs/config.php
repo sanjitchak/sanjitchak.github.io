e5c12270b5e67ff2dec08c0c5ab05aa2b38e4089
Sanjit Chakrabarti <sujitsanjit@gmail.com>
2019-09-26 19:36:20 +0530

    delete files

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"nhWS`Zvn8pG[nI0v>`EULnx3,hq%HpfZ<xj!k#-`w5,]$7ku#dcNTUUm`rEX;9NQ";s:7:"version";s:5:"1.0.4";s:13:"attackDataKey";i:763;s:11:"wafDisabled";b:0;}
